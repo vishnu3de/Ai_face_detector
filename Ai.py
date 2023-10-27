db0fc1ffe6b63bd2e3d98ff92001d3f5ae7c7b79
vishnu3de <vishnu21112001@gmail.com>
2023-10-27 21:50:19 +0530

    added basic image shower

--- Ai.py
+++ Ai.py
@@ -1,1 +1,15 @@
+import cv2
+
+#load pre-trained data from haarcascade
+trained_face_data=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+#image for detection
+image1=cv2.imread("face1.jpg")
+
+#img show
+cv2.imshow("Face detection",image1)
+
+cv2.waitKey()
+
 print("code completed")
